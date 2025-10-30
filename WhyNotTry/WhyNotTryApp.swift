fc9b42fb5aba9314ffd4bc3ed38a93368e402afe
omkar <omkarbagade16@gmail.com>
2025-10-30 07:36:59 +0530

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Omkar Vijay Bagade on 30/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
