6121256828e38ba6168adb15bca12ab8b775368c
omkar <omkarbagade16@gmail.com>
2025-10-30 09:10:05 +0530

    first commit

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities: [String] = ["Badminton", "Baseball", "Archery", "Run", "Climbing","Basketball","Boxing","Bowling","Cooldown","Dance","Fishing","Golf","Handball","Hiking","Jumprope","Pickleball"]
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+
+    @State private var selectedActivity = "Badminton"
+    @State private var id = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Why not try ...")
+                .font(Font.largeTitle.bold())
+            
+            Spacer()
+            
+            VStack{
+                Circle()
+                    .fill(colors.randomElement() ?? .red)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selectedActivity.lowercased())")
+                            .font(.system(size: 100))
+                            .foregroundColor(.white)
+                    )
+            
+                Text("\(selectedActivity)")
+                    .font(.title)
+            }
+            .transition(.scale)
+            .id(id)
+            
+            Spacer()
+            
+            Button("Try Again"){
+                withAnimation(.easeInOut(duration: 0.5)){
+                    selectedActivity = activities.randomElement() ?? "Archery"
+                    id += 1
+                }
+            }
+            .buttonStyle(.glassProminent)
         }
-        .padding()
     }
 }
 
